2c2c34a0caee2baa06af175e3a2e0c334d0ceb71
JavAndy58 <digital.designs.russia@gmail.com>
2022-03-08 12:53:15 +0300

    upp

--- src/main/java/ru/job4j/dream/controller/PostController.java
+++ src/main/java/ru/job4j/dream/controller/PostController.java
@@ -3,8 +3,11 @@ package ru.job4j.dream.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.dream.model.Post;
 import ru.job4j.dream.store.PostStore;
+
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
 
 @Controller
@@ -22,5 +25,18 @@ public class PostController {
     public String addPost(Model model) {
         model.addAttribute("post", new Post(0, "Заполните поле", "Заполните поле", LocalDate.now()));
         return "addPost";
+    }
+
+    @GetMapping("/formAddPost")
+    public String formAddPost(Model model) {
+        return "addPost";
+    }
+
+    @PostMapping("/savePost")
+    public String savePost(HttpServletRequest req) {
+        String name = req.getParameter("name");
+        System.out.println(name);
+        postService.add(new Post(1, name));
+        return "redirect:/posts";
     }
 }
